86852e9b0c0c7e11bb6e6f250f6420b93aed09ed
Kamil Marut <kamil@kamilmarut.com>
2022-06-29 21:24:18 +0200

    chore(feeds): Rewrite find_feeds script as Django command

---
+++ feeds/management/commands/find_feeds.py
@@ -0,0 +1,20 @@
+from argparse import ArgumentParser
+from typing import Any
+
+from django.core.management.base import BaseCommand
+
+from feeds.utils import find_feeds
+
+
+class Command(BaseCommand):
+    help = "Finds RSS and Atom feeds for specified URLs"
+
+    def add_arguments(self, parser: ArgumentParser) -> None:
+        parser.add_argument("url", type=str)
+
+    def handle(self, *args: Any, **options: Any) -> None:
+        url = options["url"]
+
+        feeds = find_feeds(url)
+        for feed in feeds:
+            self.stdout.write(self.style.SUCCESS('Found feed "%s" at %s' % (feed.title or "Unknown", feed.url)))
