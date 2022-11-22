1a97fdd39a10326aae8e05562416b946185955c8
Kamil Marut <kamil@kamilmarut.com>
2022-11-22 17:26:45 +0100

    feat(email): Update email templates

--- subscriptions/utils.py
+++ subscriptions/utils.py
@@ -1,6 +1,7 @@
 from django.urls import reverse
 
 from mailer.models import Message
+from nitrorss.utils.emails import render_email_template_to_string
 from nitrorss.utils.url import get_full_url
 
 from .models import Subscription
@@ -9,11 +10,18 @@ from .tokens import ConfirmSubscriptionTokenGenerator
 
 def send_subscription_confirmation_email(subscription: Subscription) -> None:
     confirmation_token = ConfirmSubscriptionTokenGenerator.make_token(obj=subscription)
-    verification_url = get_full_url(reverse("subscriptions:confirm-subscription", kwargs={"token": confirmation_token}))
 
+    context = {
+        "verification_url": get_full_url(
+            reverse("subscriptions:confirm-subscription", kwargs={"token": confirmation_token})
+        )
+    }
+    text_message = render_email_template_to_string("subscription/email/verification.txt", context)
+    html_message = render_email_template_to_string("subscription/email/verification.html", context)
     db_msg = Message.make(
         subject="Verify your subscription",
-        text_content=f"Click here to verify your subscription: {verification_url}",
+        text_content=text_message,
+        html_content=html_message,
         recipients=[subscription.target_email],
         priority=Message.Priorities.HIGH,
     )
--- users/utils.py
+++ users/utils.py
@@ -1,6 +1,7 @@
 from django.urls import reverse
 
 from mailer.models import Message
+from nitrorss.utils.emails import render_email_template_to_string
 from nitrorss.utils.url import get_full_url
 
 from .models import User
@@ -9,11 +10,16 @@ from .tokens import EmailVerificationTokenGenerator
 
 def send_verification_email(user: User) -> None:
     email_verification_token = EmailVerificationTokenGenerator.make_token(obj=user)
-    verification_url = get_full_url(reverse("users:verify-email", kwargs={"token": email_verification_token}))
 
+    context = {
+        "verification_url": get_full_url(reverse("users:verify-email", kwargs={"token": email_verification_token}))
+    }
+    text_message = render_email_template_to_string("users/email/verification.txt", context)
+    html_message = render_email_template_to_string("users/email/verification.html", context)
     db_msg = Message.make(
         subject="Verify your email",
-        text_content=f"Click here to verify your email: {verification_url}",
+        text_content=text_message,
+        html_content=html_message,
         recipients=[user.email],
         priority=Message.Priorities.HIGH,
     )
